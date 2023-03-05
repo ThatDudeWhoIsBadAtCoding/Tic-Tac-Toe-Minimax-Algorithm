cc6f936ed3a34422d132546f4fd6dbca206b67a7
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2023-03-05 23:31:21 +0530

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ class Game:
     def __init__(self, symbol_1: str, symbol_2: str) -> None:
         self.board = {'1': " ", '2': " ", '3': " ", '4': " ",
                       '5': " ", '6': " ", '7': " ", '8': " ", '9': " "}
-        assert len(symbol_1) == len(symbol_2)
+        assert len(symbol_1) == len(symbol_2) == 1 # all symbol lengths are 1
         self.player1 = symbol_1
         self.player2 = symbol_2
 
