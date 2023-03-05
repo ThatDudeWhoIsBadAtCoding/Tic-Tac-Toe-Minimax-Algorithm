cf2a162d6abd7bb9e5eed5c7d122cc4face1fed8
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2023-03-05 23:13:36 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,65 @@
+print("Welcome to TicTac Toe!")
+
+
+class Game:
+    def __init__(self, symbol_1: str, symbol_2: str) -> None:
+        self.board = {'1': " ", '2': " ", '3': " ", '4': " ",
+                      '5': " ", '6': " ", '7': " ", '8': " ", '9': " "}
+        self.player1 = symbol_1
+        self.player2 = symbol_2
+
+    def print_board(self) -> None:
+        print("---------")
+        for i in range(1, 10):
+            print(f'{self.board[str(i)]}', end="")
+            if i % 3 == 0:
+                print()
+                print("---------")
+            else:
+                print(" | ", end="")
+
+    def play_turn(self, move, player):
+        if self.board[str(move)] != " ":
+            # print(self.board[str(move)], move)
+            # self.print_board()
+            raise ValueError("Square is already occupied")
+        self.board[str(move)] = self.player1 if player else self.player2
+
+    def undo_move(self, move) -> None:
+        # board = self.board
+        self.board[str(move)] = " "
+        #  board != self.board
+
+    def check_winner(self, player) -> int:
+        # checking rows
+        for i in [1, 4, 7]:
+            if self.board[str(i)] == self.board[str(i + 1)] == self.board[str(i + 2)] and self.board[str(i)] != " ":
+                return 1 if player else -1
+
+        # checking columns
+        for i in [1, 2, 3]:
+            if self.board[str(i)] == self.board[str(i + 3)] == self.board[str(i + 6)] and self.board[str(i)] != " ":
+                return 1 if player else -1
+
+        # checking diagonals
+        if self.board["1"] == self.board["5"] == self.board["9"] != " " or self.board["3"] == self.board["5"] == self.board["7"] != " ":
+            return 1 if player else -1
+
+        if list(self.board.values()).count(" ") == 0:
+            return 0
+        return 69
+
+# game = Game("X", "O")
+# game.print_board()
+# while game.turn < 9:
+#     square = int(input("Enter your Move: "))
+#     assert 0 < square < 10  # square is between 1 and 10
+#     game.play_turn(square)
+#     if (winner := game.check_winner()) != 0:
+#         if winner == 1:
+#             print("Player wins!")
+#         else:
+#             print("Computer wins!")
+#         game.print_board()
+#         break
+#     game.print_board()
---
+++ minimax.py
@@ -0,0 +1,81 @@
+from game import Game
+from math import inf
+
+
+game = Game("X", "O")
+first_or_not = input("Enter F or first or S for second: ").lower()
+
+
+def minimax(interface, ismaximising, depth) -> int:
+    if (winner := interface.check_winner(not ismaximising)) != 69:
+        return winner, depth
+
+    if not ismaximising:
+        best_score = inf
+        all_moves = get_all_moves(interface)
+        for move in all_moves:
+            interface.play_turn(move, ismaximising)
+            score, depth = minimax(interface, not ismaximising, depth + 1)
+            interface.undo_move(move)
+            best_score = min(best_score, score)
+        return best_score, depth
+    else:
+        best_score = -inf
+        all_moves = get_all_moves(interface)
+        for move in all_moves:
+            interface.play_turn(move, ismaximising)
+            score, depth = minimax(interface, not ismaximising, depth + 1)
+            interface.undo_move(move)
+            best_score = max(best_score, score)
+        return best_score, depth
+
+
+def get_all_moves(interface):
+    possible_moves = []
+    for num, square in interface.board.items():
+        if square == " ":
+            possible_moves.append(int(num))
+    return possible_moves
+
+
+def get_best_move(interface, player):
+    best_move = None
+    best_score = inf
+    shortest_depth = inf
+    for move in get_all_moves(interface):
+        interface.play_turn(move, player)
+        score, depth = minimax(interface, not player, 0)
+        if score <= best_score:
+            if depth < shortest_depth:
+                best_score = score
+                best_move = move
+        # print(f"Move = {move}, Score = {score}, Depth = {depth}")
+        interface.undo_move(move)
+    return best_move
+
+
+player = False
+
+if first_or_not == "f":
+    player = True
+    game.print_board()
+
+while get_all_moves(game):
+    if player:
+        square = int(input("Enter your Move: "))
+    else:
+        print("Computer Plays its move")
+        square = get_best_move(game, player)
+    assert 0 < square < 10  # square is between 1 and 10
+    game.play_turn(square, player)
+    if (winner := game.check_winner(player)) != 69:  # if 69 then no winner yet
+        if winner == 1:
+            print("Player wins!")
+        elif winner == 0:
+            print("Tie!")
+        else:
+            print("Computer wins!")
+        game.print_board()
+        break
+    player = not player
+    game.print_board()
